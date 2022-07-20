aee3f5923184c4cafaf9e3aa557061f1734b7b96
Anderson Lima <andersonplima@gmail.com>
2022-07-19 23:45:25 -0300

    Sticker montado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.io.File;
+import java.io.FileInputStream;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        new StickersGenerator().cria();
+        new StickersGenerator()
+            .cria(new FileInputStream(
+                new File("entrada/filme.jpg")), 
+                "saida/filme.png",
+                new String[] {"Tô aqui", "pensando na merda", "que você falou..."} );
     }
 }
--- src/StickersGenerator.java
+++ src/StickersGenerator.java
@@ -1,29 +1,46 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.imageio.ImageIO;
 
 public class StickersGenerator {
-    public void cria() throws IOException {
-        var image = ImageIO.read(new File("entrada/filme.jpg"));
-        var width = image.getWidth();
-        var height = image.getHeight();
+    public void cria(InputStream inputStream, String outputPath, String[] message) throws IOException {
+        var image = ImageIO.read(inputStream);
+        var width = 640;
+        var height = 480;
+        var textAreaHeight = 120;
+        var imageHeight = height - textAreaHeight;
+        var frameThickness = 5;
+        var numberOfLines = 3;
+        var lineSpace = 6;
+        var fontSize = textAreaHeight/numberOfLines;
 
-        var newHeight = height + 200;
-
-        var sticker = new BufferedImage(width, newHeight, BufferedImage.TYPE_4BYTE_ABGR);
-        Graphics2D graphics2d = (Graphics2D)sticker.getGraphics();
-        graphics2d.drawImage(image, 0, 0, null);
+        var sticker = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+        Graphics2D graphics2d = (Graphics2D) sticker.getGraphics();
+        graphics2d.drawImage(image, 0, 0, width, imageHeight, null);
         graphics2d.setColor(Color.red);
-        graphics2d.setStroke(new BasicStroke(10));
-        graphics2d.drawRect(0, 0, width, newHeight);
+        graphics2d.setStroke(new BasicStroke(frameThickness));
+        graphics2d.drawRect(0, 0, width, height);
         graphics2d.setColor(Color.yellow);
-        graphics2d.drawRect(10, 10, width - 20, newHeight - 20);
+        graphics2d.drawRect(frameThickness, frameThickness, width - 2 * frameThickness, height - 2 * frameThickness);
+        var font = new Font(Font.MONOSPACED, Font.BOLD, fontSize);
+        graphics2d.setFont(font);
+        var fontMetrics = graphics2d.getFontMetrics(font);
+        var i = 0;
+        for (String string : message) {
+            var stringBounds = fontMetrics.getStringBounds(string, graphics2d);
+            var stringWidth = (int) stringBounds.getWidth();
+            var stringHeight = (int) stringBounds.getHeight();
+            graphics2d.drawString(string, (width - stringWidth) / 2, imageHeight + (i + 1) * (lineSpace + stringHeight/2));
+            ++i;
+        }
 
-        ImageIO.write(sticker, "png", new File("saida/filme.png"));
+        ImageIO.write(sticker, "png", new File(outputPath));
     }
 }
